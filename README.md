2dfaceb4a601bdb25d3ab0ef6e0c9825fa4c8f6e
uladzimir-miadzinski <webmedinsky@gmail.com>
2023-07-17 11:37:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-event-emitter-task
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mj9qra)
---
+++ index.js
@@ -0,0 +1,22 @@
+// EventEmitter
+class EventEmitter {}
+
+const cb1 = () => console.log('cb1');
+const cb2 = () => console.log('cb2');
+const cb3 = () => console.log('cb3');
+
+
+const emitter = new EventEmitter();
+
+emitter
+  .on('click', cb1)
+  .on('click', cb2)
+  .emit('click')
+  .on('keyup', cb3)
+  .off('click', cb2)
+  .emit('click')
+
+// console:
+// cb1
+// cb2
+// cb1
